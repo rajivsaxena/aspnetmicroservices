de1c384f45fea4f935b8e194f62db682bf7e7cd7
Rajiv Saxena <rajiv.saxena@mankindpharma.com>
2022-05-12 01:11:41 +0530

    Added Postgres Database Migration code

---
+++ src/Services/Discount/Discount.API/Extensions/HostExtension.cs
@@ -0,0 +1,56 @@
+﻿using Npgsql;
+
+namespace Discount.API.Extensions
+{
+    public static class HostExtension
+    {
+        public static IHost MigrateDatabase<TContext>(this IHost host, int ? retry=0)
+        {
+            int retryForAvailibility = retry.Value;
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var configuration = services.GetRequiredService<IConfiguration>();
+                var logger = services.GetRequiredService<ILogger<TContext>>();
+                try
+                {
+                    logger.LogInformation("Migrating Postgre database.");
+                    
+                    using var connection = new NpgsqlConnection(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+                    connection.Open();
+                    
+                    using var command = new NpgsqlCommand { Connection = connection };
+                    command.CommandText = "Drop table if exists Coupon";
+                    command.ExecuteNonQuery();
+
+                    command.CommandText = @"Create table coupon(Id SERIAL PRIMARY KEY,
+                                                                ProductName Varchar(24) Not Null,
+                                                                Description Text,
+                                                                Amount Int)";
+                    command.ExecuteNonQuery();
+
+                    command.CommandText = "Insert into coupon (ProductName, Description, Amount) Values ('IPhone X', 'IPhone X Desc', 199)";
+                    command.ExecuteNonQuery();
+
+                    command.CommandText = "Insert into coupon (ProductName, Description, Amount) Values ('Samsung 10', 'Samsung 10 Desc', 99)";
+                    command.ExecuteNonQuery();
+
+                    logger.LogInformation("Migration of Postgre database completed.");
+
+                }
+                catch (NpgsqlException ex)
+                {
+                    logger.LogError(ex, "An error occured while migrating the database");
+                    if(retryForAvailibility < 50)
+                    {
+                        retryForAvailibility++;
+                        System.Threading.Thread.Sleep(200);
+                        MigrateDatabase<TContext>(host, retryForAvailibility);
+                    }
+                }
+                return host;
+            }
+        }
+            
+    }
+}
--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,13 +1,17 @@
+using Discount.API.Extensions;
+using Discount.API.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IDiscountRepository,DiscountRepository>();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -19,5 +23,5 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
-
+app.MigrateDatabase<Program>();
 app.Run();
