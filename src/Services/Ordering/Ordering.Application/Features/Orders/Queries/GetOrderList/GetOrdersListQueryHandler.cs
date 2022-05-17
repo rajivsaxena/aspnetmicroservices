4f4350fdb273767148787e9220cde1b90df62b5a
Rajiv Saxena <rajiv.saxena@mankindpharma.com>
2022-05-17 13:53:41 +0530

    Developing Ordering.Application feature GetOrderdListQuery

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrderList/GetOrdersListQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Contracts.Persistance;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrderList
+{
+    public class GetOrdersListQueryHandler : IRequestHandler<GetOrdersListQuery, List<OrdersVm>>
+    {
+        private IOrderRepository _orderRepository;
+        private IMapper _mapper;
+
+        public GetOrdersListQueryHandler(IOrderRepository orderRepository, IMapper mapper)
+        {
+            _orderRepository = orderRepository ?? throw new ArgumentNullException(nameof(orderRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        public async Task<List<OrdersVm>> Handle(GetOrdersListQuery request, CancellationToken cancellationToken)
+        {
+            var orderList = await _orderRepository.GetOrdersByUserName(request.UserName);
+            return _mapper.Map<List<OrdersVm>>(orderList);
+        }
+    }
+}
