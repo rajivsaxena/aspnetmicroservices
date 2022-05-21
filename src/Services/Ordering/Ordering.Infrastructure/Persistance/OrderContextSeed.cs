9d238aa2302bc1c9948a5b2d2fa5a139627c17ae
Rajiv Saxena <rajiv.saxena@mankindpharma.com>
2022-05-22 00:52:45 +0530

    Clean Architecture : Developed Ordering.Infrastructure, InfrastructureService registration, adding Application and Infra layer dependency into ordering.API

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistance/OrderContextSeed.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Infrastructure.Persistance
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.AddRange(GetPreconfiguredOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation("Seed database associated with context {DbContextName}", typeof(OrderContext).Name);
+            }
+        }
+
+        private static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            return new List<Order>
+            {
+                new Order 
+                {
+                    UserName="rajiv",
+                    LastName="saxena",
+                    EmailAddress="rajiv.saxena1784@gmail.com",
+                    AddressLine="delhi", 
+                    TotalPrice=350
+                }
+            };
+        }
+    }
+}
+
